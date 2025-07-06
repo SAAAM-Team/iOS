ee35ae8320faeb14cebcde3cf40e6a453d7b3449
saber-be <saberbejestani@gmail.com>
2025-07-06 14:42:55 +0330

    all other files

---
+++ Il2CppOutputProject/Source/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	12634,
+	g_Il2CppGenericTypes,
+	8797,
+	g_Il2CppGenericInstTable,
+	115849,
+	g_Il2CppGenericMethodFunctions,
+	36013,
+	g_Il2CppTypeTable,
+	133769,
+	g_Il2CppMethodSpecTable,
+	9595,
+	g_FieldOffsetTable,
+	9595,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
